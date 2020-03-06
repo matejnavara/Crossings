cd4615707864097541c850fd945de3cd00616572
Matej <matej.navara@gmail.com>
2020-03-06 17:09:30 +0000

    Spawning cars

--- Assets/Scripts/CarScript.cs
+++ Assets/Scripts/CarScript.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class CarScript : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float speed = 10.0f;
+
+    void OnTriggerEnter(Collider col)
+    {
+            Debug.Log("Collide of " + gameObject.name + " and " + col.name);
+            speed = col.gameObject.GetComponentInParent<CarScript>().speed;
+            Debug.Log("New speed = " + speed);
+    }
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/CarSpawnScript.cs
+++ Assets/Scripts/CarSpawnScript.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class CarSpawnScript : MonoBehaviour
 {
-    public GameObject car;
+    public GameObject[] cars;
+    
+    private float respawnTimer;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(car, transform.position, Quaternion.identity);
+        respawnTimer = Random.Range(2.0f, 5.0f);
+    }
+
+    void SpawnCar()
+    {
+        int randomCar = Random.Range(0,7);
+        Debug.Log("Spawning car: " + randomCar);
+        Instantiate(cars[randomCar], transform.position, transform.rotation);
+        respawnTimer = Random.Range(3.0f, 15.0f);;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        respawnTimer -= Time.deltaTime;
+ 
+         if (respawnTimer <= 0.0f) {
+            SpawnCar();
+         }
     }
 }
