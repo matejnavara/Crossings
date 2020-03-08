6cd63ca9d2de47d10ad2e32a13df6fb23ac287bb
Matej <matej.navara@gmail.com>
2020-03-08 04:28:53 +0000

    Redman animating

--- Assets/Scripts/CrosserScript.cs
+++ Assets/Scripts/CrosserScript.cs
@@ -7,10 +7,12 @@ public class CrosserScript : MonoBehaviour
     public bool isWaiting = false;
     public GameObject explodeParticles;
     private GameManager gm;
+    private Animator anim;
 
     void Start()
     {
         gm = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+        anim = GetComponent<Animator> ();
     }
 
     void OnTriggerEnter(Collider col)
@@ -19,6 +21,8 @@ public class CrosserScript : MonoBehaviour
         if (col.tag == "WaitZone")
         {
             isWaiting = true;
+            anim.Play("Waiting", 0, Random.Range(0f, 1f));
+            // anim.SetBool("isWaiting", true);
         };
 
         if (col.tag == "Finish")
@@ -41,6 +45,8 @@ public class CrosserScript : MonoBehaviour
         if (col.tag == "WaitZone")
         {
             isWaiting = false;
+            anim.Play("Walking", 0, Random.Range(0f, 1f));
+            //anim.SetBool("isWaiting", false);
         };
     }
 
