a76a4f617f96e740088f7043df5d48083130a0bb
Matej <matej.navara@gmail.com>
2020-03-09 07:17:46 +0000

    Leaderboard progress

---
+++ Assets/Scripts/GameOverFade.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverFade : MonoBehaviour
+{
+    public GameObject gameoverScreen;
+
+    void Start()
+    {
+        gameoverScreen.SetActive(false);
+    }
+    public void DisplayGameover()
+    {
+        gameoverScreen.SetActive(true);
+    }
+}
