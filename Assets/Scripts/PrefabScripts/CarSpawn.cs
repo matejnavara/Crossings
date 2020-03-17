27d905d666c3cb6006c8829a65b6c865d9344914
Matej <matej.navara@gmail.com>
2020-03-17 10:27:09 +0000

    Seperate normal/emergency car spawns

--- Assets/Scripts/PrefabScripts/CarSpawn.cs
+++ Assets/Scripts/PrefabScripts/CarSpawn.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CarSpawn : MonoBehaviour
 {
-    public GameObject[] cars;
+    public GameObject[] normalCars;
+    public GameObject[] emergencyCars;
     
     private GameManager gm;
     private float respawnTimer;
@@ -17,14 +18,37 @@ public class CarSpawn : MonoBehaviour
 
     void SpawnCar()
     {
-        int randomCar = Random.Range(0,20);
         if (gm.getRushHour())
         {
+            SelectSpawn(0.8f);
             respawnTimer = Random.Range(1.0f, 3.0f);
         } else {
+            SelectSpawn(0.95f);
             respawnTimer = Random.Range(3.0f, 10.0f);
         }
-        Instantiate(cars[randomCar], transform.position, transform.rotation);
+    }
+
+    void SelectSpawn(float threshold)
+    {
+        float emergencyChance = Random.Range(0f, 1f);
+        if (emergencyChance > threshold) {
+            Debug.Log("EMERGENCY: " + emergencyChance);
+            SpawnEmergencyCar();
+        } else {
+            SpawnNormalCar();
+        }
+    }
+
+    void SpawnNormalCar()
+    {
+        int randomCar = Random.Range(0, normalCars.Length);
+        Instantiate(normalCars[randomCar], transform.position, transform.rotation);
+    }
+
+    void SpawnEmergencyCar()
+    {
+        int randomCar = Random.Range(0, emergencyCars.Length);
+        Instantiate(emergencyCars[randomCar], transform.position, transform.rotation);
     }
 
     // Update is called once per frame
@@ -32,7 +56,7 @@ public class CarSpawn : MonoBehaviour
     {
         respawnTimer -= Time.deltaTime;
  
-         if (respawnTimer <= 0.0f) {
+         if (!gm.getGameOver() && respawnTimer <= 0.0f) {
             SpawnCar();
          }
     }
